7ce8de518243633533ade1f32b17f69880f205ec
Adeeb Jamal <adeeb.jamal186@outlook.com>
2024-01-15 19:05:51 +0530

    updated Authenticate.ts

---
+++ Interfaces/userInterface.ts
@@ -0,0 +1,7 @@
+interface UserInterface {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export default UserInterface;
--- Routes/Authenticate.ts
+++ Routes/Authenticate.ts
@@ -2,12 +2,34 @@ import express, {Request, Response, Router} from "express";
 
 const router: Router = express.Router();
 
+// Importing user defined files and functions
+import User from "../Models/Users";
+import userInterface from "../Interfaces/userInterface";
+
 router.post("/register", async(req: Request, res: Response) => {
     try {
         console.log(req.body);
-        return res.status(200).render("homepage", {
-            message: ""
-        });
+        if(!req.body.newName || !req.body.newEmail || !req.body.newPassword || !req.body.confirmpassword) {
+            return res.status(400).render("homepage", {
+                message: "Please fill all the required fields"
+            });
+        }
+        if(req.body.newpassword !== req.body.confirmPassword) {
+            return res.status(400).render("homepage", {
+                message: "Passwords doesn't match."
+            });
+        }
+        if(req.body.newPassword.length < 6) {
+            return res.status(400).render("homepage", {
+                message: "Password is too weak."
+            });
+        }
+        const foundUser: userInterface | null = await User.findOne({email: req.body.newEmail});
+        if(foundUser) {
+            return res.status(400).render("homepage", {
+                message: "User with emtered email already exists."
+            });
+        }
     }
     catch(error) {
         console.log(error);
