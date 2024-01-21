867fb60a711635da1635da9f9a0819963a74ea80
Adeeb Jamal <adeeb.jamal186@outlook.com>
2024-01-21 15:56:04 +0530

    Logged in users will be redirected to the dashboard without filling the credentials.

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
 import express, {Express, Request, Response, NextFunction} from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
+// Importing user defined files and functions
+import User from "./Models/Users";
+import userInterface from "./Interfaces/userInterface";
 
 const app: Express = express();
 
@@ -19,6 +24,15 @@ app.use("/authenticate", require("./Routes/Authenticate"));
 
 app.get("/", async(req: Request, res: Response) => {
     try {
+        if(req.cookies.loggedin_user) {
+            const decoded_JWT: {ID: string} = jwt.verify(req.cookies.loggedin_user, "Secret-Key");
+            const foundUser: userInterface | null = await User.findOne({_id: decoded_JWT.ID});
+            if(foundUser) {
+                return res.status(200).render("homepage", {
+                    name: foundUser.name
+                });
+            }
+        }
         return res.status(200).render("homepage", {
             message: ""
         });
